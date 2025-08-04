9003e55d75b31061103a986b051d21249f6505c4
qblmchmmd <qblmchmmd.dev@gmail.com>
2025-08-05 06:01:55 +0700

    input command add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add { task_name: String },
+}
+
+fn add_task(task_name: String) {
+    println!("Add Task {}", task_name)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Add { task_name } => add_task(task_name),
+    }
 }
